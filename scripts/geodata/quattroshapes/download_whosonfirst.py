f6091813c177a897d642c934f836200f63d13f52
Yuyang Shu <yuyang.shu@wisetechglobal.com>
2020-04-20 16:39:09 +1000

    place types

--- scripts/geodata/quattroshapes/download_whosonfirst.py
+++ scripts/geodata/quattroshapes/download_whosonfirst.py
@@ -1,3 +1,6 @@
+import collections
+import fnmatch
+import json
 import os
 import subprocess
 
@@ -14,8 +17,19 @@ each country's data has its own repository, this script just clones all of them
 if __name__ == "__main__":
     for country in pycountry.countries:
         repo = "whosonfirst-data-admin-{}".format(country.alpha2.lower())
-        if not os.path.exists(repo):
-            try:
-                subprocess.call(["git", "clone", "https://github.com/whosonfirst-data/{}".format(repo)])
-            except:
-                print "no data for {}".format(repo[:-2])
+        if not os.path.exists(os.path.join("data", "whosonfirst", repo)):
+            subprocess.call(["git", "clone", "https://github.com/whosonfirst-data/{}".format(repo)])
+    
+    counter = collections.defaultdict(int)
+    matches = []
+    for root, dirnames, filenames in os.walk(os.path.join("data", "whosonfirst")):
+        for filename in fnmatch.filter(filenames, "*.geojson"):
+            matches.append(os.path.join(root, filename))
+            # print matches[-1]
+    for fname in matches:
+        with open (fname) as f:
+            geojson = json.load(f)
+        if not geojson["properties"].get("wof:superseded_by", ""):
+            counter[geojson["properties"].get("wof:placetype", "undefined")] += 1
+    
+    print counter 
